e54562d291c23ab43cb0f352b9599beddff7ce89
Aditya raj <meadityaraj0001@gmail.com>
2023-12-14 19:14:04 +0530

    finish loader add nav for large devices

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,9 @@
 "use client";
 import Link from "next/link";
+import { MdOutlineArrowDropDownCircle } from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
+import { FaBars } from "react-icons/fa";
+import { BiUpArrow } from "react-icons/bi";
 import { useRef, useLayoutEffect, useState, useEffect } from "react";
 import gsap from "gsap";
 import { motion } from "framer-motion";
@@ -7,13 +11,32 @@ const TARGET_TEXT = "Advitiya 2024";
 const CYCLES_PER_LETTER = 2;
 const SHUFFLE_TIME = 50;
 const CHARS = "!@#$%^&*():{};|,.<>/?";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+// const DropdownIcon=({dropdownclicked})=>{
+
+//   return (
+//     <>
+//     {dropdownclicked?<BiUpArrow />:<MdOutlineArrowDropDownCircle className="text-white" />}
+//     </>
+    
+//     )
+// }
 
 export default function intro() {
   const firstref = useRef(null);
   const intervalRef = useRef(null);
   const tl = useRef();
   const [text, setText] = useState(TARGET_TEXT);
+  const [dropdownclicked, setdropdownclicked] = useState(false)
 
   const scramble = () => {
     let pos = 0;
@@ -50,50 +73,59 @@ export default function intro() {
   };
 
   useLayoutEffect(() => {
+
     let ctx = gsap.context(() => {
-      tl.current = gsap
-        .timeline()
-        // .from("#loader h1", {
-        //   delay: 1,
-        //   x: 40,
-        //   opacity: 0,
-        //   duration: 1,
-        //   stagger: 0.1,
-        // })
-        // .to("#loader h1", {
-        //   opacity: 0,
-        //   x: -40,
-        //   duration: 1,
-        //   stagger: 0.1,
-        // })
-        // .to("#loader", {
-        //   opacity: 0,
-        // })
-        // .to("#loader", {
-        //   onStart: () => {
-        //     document.querySelector("#loader").style.display = "none";
-        //   },
-        // })
-        // .from("#intro h1", {
-        //   opacity: 0,
-        //   duration: 1,
-        // })
-        // .from("#intro h1", {
-        //   delay:0.5,
-        //   onStart: scramble,
-        // })
-        // .to("#intro h1",{
-        //   // delay:1,
-        //   // opacity:0,
-        //   // duration:1,
-        //   onComplete: () => {
-        //     document.querySelector("#intro").style.display = "none";
-        //   },
-        // })
-        // .from("#home",{
-        //   // duration:1,
-        //   opacity:1
-        // })
+      tl.current = gsap.timeline()
+      .from("#loader h1", {
+        onStart:()=>{
+          document.querySelector("#loader").style.display="flex"
+        },
+        delay: 1,
+        x: 40,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.1,
+      })
+      .to("#loader h1", {
+        opacity: 0,
+        x: -40,
+        duration: 1,
+        stagger: 0.1,
+      })
+      .to("#loader", {
+        opacity: 0,
+        onComplete: () => {
+          document.querySelector("#loader").style.display = "none";
+          document.querySelector("#intro").style.display = "flex";
+        }
+      })
+      .from("#intro h1", {
+        opacity: 0,
+        duration: 1,
+      })
+      .from("#intro h1", {
+        delay:0.5,
+        onStart: scramble,
+      })
+      .to("#intro h1",{
+        delay:1,
+        opacity:0,
+        duration:1,
+        onComplete: () => {
+          document.querySelector("#intro").style.display = "none";
+          document.querySelector("#page1").style.display="block"
+        },
+      })
+      .from("#home",{
+        // duration:1,
+        opacity:1
+      })
+      .from("nav #btn",{
+        y:-100,
+        opacity:0,
+        duration:0.5,
+        stagger:0.2
+      })
     }, firstref);
 
     return () => ctx.revert();
@@ -102,20 +134,20 @@ export default function intro() {
   return (
     <>
       <div id="main " ref={firstref}>
-        {/* <div
+        <div
           id="loader"
-          className="text-indigo-300 h-[100vh] w-[100vw] bg-black z-2 flex items-center justify-center "
+          className="text-indigo-300 h-[100vh]  w-[100vw] bg-black z-2 hidden  items-center justify-center "
         >
           <div className="flex flex-col gap-2 justify-center items-center md:flex-row text-xl  md:text-2xl lg:text-3xl ">
             <h1>Welcome to</h1>
             <h1>the</h1>
             <h1>Future</h1>
           </div>
-        </div> */}
+        </div> 
 
-        {/* <div
+         <div
           id="intro"
-          className="bg-black  text-xl md:text-3xl lg:text-5xl flex h-[100vh] items-center justify-center"
+          className="bg-black text-xl md:text-3xl lg:text-5xl hidden h-[100vh] items-center justify-center"
         >
           <motion.h1
             // whileHover={{
@@ -146,16 +178,43 @@ export default function intro() {
               className="animatedbar duration-300 absolute inset-0 z-0 scale-125 bg-gradient-to-t from-indigo-400/0 from-40% via-indigo-400/100 to-indigo-400/0 to-60% opacity-0 transition-opacity group-hover:opacity-100"
             />
           </motion.h1>
-        </div> */} 
+        </div>  
 
-        <div id="page1" className="home bg-white relative h-[100vh] w-[100%]">
+        <div id="page1" className="home bg-white hidden relative h-[100vh] w-[100%]">
           {/* <video src="./aftermovie.mp4" autoPlay loop muted className="h-[100%] w-[100%] object-cover absolute"></video> */}
           <div className="page1-content h-[100%] w-[100%] relative text-white ">
             <nav className="flex items-center justify-between px-[2vw] py-[4vh]">
-            <Button></Button>
-     
+              <span id="btn">
+
+              <DropdownMenu id="btn">
+                <DropdownMenuTrigger className="flex flex-row items-center gap-2 outline-none ">Events<MdOutlineArrowDropDownCircle/></DropdownMenuTrigger>
+                <DropdownMenuContent className="text-white">
+                  <DropdownMenuItem>Hackathons</DropdownMenuItem>
+                  <DropdownMenuItem>Workshops</DropdownMenuItem>
+                  <DropdownMenuItem>Exhibitions</DropdownMenuItem>
+                  <DropdownMenuItem>Talk Shows</DropdownMenuItem>
+                  <DropdownMenuItem>Competitions</DropdownMenuItem>
+                  <DropdownMenuItem>Robowars</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+              </span>
+              <Button id="btn" variant="ghost">Schedule</Button>
+              <Button id="btn" variant="ghost">Accommodation</Button>
+              <Button id="btn" variant="ghost">Sponsors</Button>
+              <Button id="btn" variant="ghost">Inter-School-Conclave</Button>
+              <Button id="btn" variant="ghost">Contact Us</Button>
+              <Button id="btn" variant="ghost">About Us</Button>
+              <Button id="btn" variant="ghost">FAQs</Button>
+              <span id="btn"><DropdownMenu id="btn">
+                <DropdownMenuTrigger className="flex flex-row items-center gap-2 outline-none text-2xl"> <CgProfile/><FaBars/> </DropdownMenuTrigger>
+                <DropdownMenuContent className="text-white mt-2 mr-2">
+                  <DropdownMenuItem>Profile</DropdownMenuItem>
+                  <DropdownMenuItem>Login/Register</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu></span>
             </nav>
           </div>
+
         </div>
       </div>
     </>
