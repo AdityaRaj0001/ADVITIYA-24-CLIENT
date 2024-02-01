600b8a4ff498a0334da5c947c705f4f4aab705b9
Aditya raj <meadityaraj0001@gmail.com>
2024-02-01 19:34:03 +0530

    minor bugs

--- src/components/our-videos/OurVideos2.jsx
+++ src/components/our-videos/OurVideos2.jsx
@@ -21,15 +21,16 @@ const OurVideos2 = () => {
           
           <a className="block mt-4 underline text-[#cd71f1] text-md" target="_blank" href="https://www.instagram.com/p/C2e45pgRqy0/">Click to view</a>
 </p>
-          <img className="absolute pointer-events-none w-full max-w-sm lg:max-w-lg" src="./androidrect.svg"/>
+          <img className="absolute  pointer-events-none w-full max-w-sm lg:max-w-lg" src="./androidrect.svg"/>
         </div>
       </div>
       <div className="h-[70vh] flex flex-col gap-12   sm:flex-row w-[100%] sm:h-[50vh]">
         <div className="videodiv relative flex items-center justify-center w-[100%] h-[50%] sm:h-[100%]">
           <iframe   src="https://www.youtube.com/embed/Fwn-ZAxCq4g?si=vQsd07LBv1Df6vFr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className="h-[100%] sm:w-[80%] "></iframe>
         </div>
-        <div className="relative flex items-start sm:items-center justify-center w-[100%] h-[30%] sm:h-[100%]">
-          <p className="px-4 max-w-sm lg:max-w-lg text-white text-sm py-4 lg:text-lg ">Step into the captivating realm of innovation and excitement with our Tech Fest After Movie! Relive the vibrant energy, groundbreaking tech showcases, and unforgettable moments from our <span className="text-[#cd71f1]">previous campus extravaganza.</span> </p>
+        <div className="relative mb-12 flex items-start sm:items-center justify-center w-[100%] h-[30%] sm:h-[100%]">
+          <p className="block md:hidden  px-4 max-w-sm lg:max-w-lg text-white text-sm py-4 lg:text-lg ">Step into the captivating realm of innovation and excitement with our Tech Fest After Movie! Relive the unforgettable moments from our <span className="text-[#cd71f1]">previous campus extravaganza.</span></p>
+          <p className="hidden md:block px-4 max-w-sm lg:max-w-lg text-white text-sm py-4 lg:text-lg ">Step into the captivating realm of innovation and excitement with our Tech Fest After Movie! Relive the vibrant energy, groundbreaking tech showcases, and unforgettable moments from our <span className="text-[#cd71f1]">previous campus extravaganza.</span> </p>
           <img className="absolute w-full max-w-sm lg:max-w-lg" src="./androidrect.svg"/>
         </div>
       </div>
